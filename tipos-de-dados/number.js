18a8cd40b8b3da3be25c7bebf2c8fe964fee821b
cortelucas <corte1994.lc@gmail.com>
2021-02-22 16:03:56 -0300

    numbers

---
+++ tipos-de-dados/number.js
@@ -0,0 +1,16 @@
+/*
+Number = numeros
+
+podemos ter:
+    - 33   // inteiros
+    - 12.5 // float
+    - NaN  // Not a Number
+    - Infinity // infinito
+*/
+
+console.log(`
+    O tipo de 33 é = ${typeof (33)}
+    O tipo de 12.5 é = ${typeof (12.5)}
+    12/abc = ${typeof (12 / 'abc')}
+    Infinito é igual a = ${Infinity}
+`);
