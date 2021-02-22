44c6580af6a0801d227f0a2807e354c375ef344f
cortelucas <corte1994.lc@gmail.com>
2021-02-22 15:45:12 -0300

    -strings

---
+++ tipos-de-dados/strings.js
@@ -0,0 +1,15 @@
+/*
+String é uma cadeia de caracteres,
+normamel elas são colocadas entre
+"" // aspas duplas
+'' // aspas simples
+`` // templates literals ou template strings
+*/
+
+console.log("Hello, World!");
+console.log('Hello, World!');
+console.log(`Hello, World!`);
+
+console.log('Hello, "World"');
+console.log(`O bom do template literals é que,
+podemos escrever em multilinhas`);
