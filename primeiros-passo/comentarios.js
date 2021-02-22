d82363dbdbb0e22ac13ba4bb7a45186800a031df
cortelucas <corte1994.lc@gmail.com>
2021-02-22 15:27:52 -0300

    -primeiro commit

---
+++ primeiros-passo/comentarios.js
@@ -0,0 +1,9 @@
+//Aqui eu tenho um comentario de uma linha
+
+/*
+Aqui tenho um comentario multilinha;
+
+Abaixo, um código que escreve mensagens no console.
+*/
+
+console.log('Bem vindo ao Starter!');
